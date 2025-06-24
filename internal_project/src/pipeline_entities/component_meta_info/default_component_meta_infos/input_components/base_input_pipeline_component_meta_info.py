eeecb251e55f3bba0c470f3b4652d1b18ac9dac0
JannikGabler <jannik@mail-gabler.de>
2025-06-24 10:54:33 +0200

    [I]: Added meta infos for default components * base_input_pipeline_component_meta_info.py * function_callable_input_component_meta_info.py * function_value_input_component_meta_info.py * multiple_function_expressions_input_component_meta_info.py * single_function_expression_input_component_meta_info.py * dummy_pipeline_component_meta_info.py * equidistant_node_generator_meta_info.py

---
+++ internal_project/src/pipeline_entities/component_meta_info/default_component_meta_infos/input_components/base_input_pipeline_component_meta_info.py
@@ -0,0 +1,12 @@
+from pipeline_entities.component_meta_info.dataclasses.component_meta_info import ComponentMetaInfo
+
+
+base_input_pipeline_component_meta_info: ComponentMetaInfo = ComponentMetaInfo(
+    attributes_modifying={"data_type", "node_count", "interpolation_interval"},
+
+    dynamic_constraints=[],
+
+    static_constraints=[],
+
+    mixed_constraints=[]
+)
---
+++ internal_project/src/pipeline_entities/component_meta_info/default_component_meta_infos/input_components/single_function_expression_input_component_meta_info.py
@@ -0,0 +1,17 @@
+from pipeline_entities.component_meta_info.dataclasses.component_meta_info import ComponentMetaInfo
+from pipeline_entities.constraints.default_constraints.default_dynamic_constraints.input_key_required_constraint import \
+    InputKeyRequiredConstraint
+from pipeline_entities.constraints.default_constraints.default_static_constraints.attribute_required_constraint import \
+    AttributeRequiredConstraint
+
+single_function_expression_input_component_meta_info: ComponentMetaInfo = ComponentMetaInfo(
+    attributes_modifying={"function_values"},
+
+    dynamic_constraints=[InputKeyRequiredConstraint("single_function_expression"),
+                         InputKeyRequiredConstraint("sympy_function_expression_simplification")],
+
+    static_constraints=[AttributeRequiredConstraint("data_type"), AttributeRequiredConstraint("node_count"),
+                        AttributeRequiredConstraint("nodes")],
+
+    mixed_constraints=[]
+)
