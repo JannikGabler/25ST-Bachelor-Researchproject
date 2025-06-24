c90ac44ae99ed139057cb7a97f678e5e202409d9
JannikGabler <jannik@mail-gabler.de>
2025-06-24 10:57:50 +0200

    [I]: Added pipeline_data

---
+++ internal_project/src/pipeline_entities/data_transfer/pipeline_data.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Optional, Tuple, Any
+import jax.numpy as jnp
+
+@dataclass
+class PipelineData:
+    data_type: Optional[type] = None
+
+    node_count: Optional[int] = None
+    interpolation_interval: Optional[jnp.ndarray] = None
+    function_values: Optional[jnp.ndarray] = None
+
+    nodes: Optional[jnp.ndarray] = None
+
+    additional_values: dict[str, Any] = field(default_factory=dict)
+
+
+
+
+
