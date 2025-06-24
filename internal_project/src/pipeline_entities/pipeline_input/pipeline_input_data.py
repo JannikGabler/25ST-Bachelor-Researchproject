a9212cc4359bc83e404db5214030e3a1381bdff6
JannikGabler <jannik@mail-gabler.de>
2025-06-24 10:55:55 +0200

    [I]: Added pipeline_input & pipeline_input_data

---
+++ internal_project/src/pipeline_entities/pipeline_input/pipeline_input_data.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class PipelineInputData:
+    name: str | None = None
+
+    data_type: str | None = None
+    node_count: str | None = None
+    interpolation_interval: str | None = None
+
+    single_function_expression: str | None = None
+    multiple_function_expressions: str | None = None
+    sympy_function_expression_simplification: str | None = None
+    function_callable: str | None = None
+    function_values: str | None = None
+
+    additional_directly_injected_values: dict[str, str] = field(default_factory=dict)
+    additional_values: dict[str, str] = field(default_factory=dict)
+
+
+
