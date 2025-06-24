bb0617aebe5f53906eb74308a4d0a680c0010bed
JannikGabler <jannik@mail-gabler.de>
2025-06-24 10:47:42 +0200

    [I]: Added default test pipeline components * dummy_pipeline_component.py

---
+++ internal_project/src/pipeline_entities/components/default_components/default_test_components/__register_default_test_components__.py
@@ -0,0 +1,4 @@
+### DO NOT REMOVE IMPORTS, THEY ARE NECESSARY!  ###
+
+def register_default_test_components():
+    from . import dummy_pipeline_component
---
+++ internal_project/src/pipeline_entities/components/default_components/default_test_components/dummy_pipeline_component.py
@@ -0,0 +1,23 @@
+from pipeline_entities.component_meta_info.default_component_meta_infos.test_components.dummy_pipeline_component_meta_info import \
+    dummy_pipeline_component_meta_info
+from pipeline_entities.components.abstracts.node_generator import NodeGenerator
+
+from pipeline_entities.components.abstracts.pipeline_component import PipelineComponent
+from pipeline_entities.components.decorators.pipeline_component import pipeline_component
+
+
+@pipeline_component(id="dummy", type=PipelineComponent, meta_info=dummy_pipeline_component_meta_info)
+class DummyPipelineComponent(NodeGenerator):
+    ######################
+    ### Public methods ###
+    ######################
+    def perform_action(self) -> None:
+        pass
+
+
+
+    ##########################
+    ### Overridden methods ###
+    ##########################
+    def __repr__(self) -> str:
+        return "Dummy pipeline component with no functionality"
