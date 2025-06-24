1ef99500928fe80724e549ff73c4250b8fbc6758
JannikGabler <jannik@mail-gabler.de>
2025-06-24 10:42:19 +0200

    [I]: Added input_key_required_constraint.py as default constraint

---
+++ internal_project/src/pipeline_entities/constraints/default_constraints/default_dynamic_constraints/input_key_required_constraint.py
@@ -0,0 +1,29 @@
+from dataclasses import fields
+
+from pipeline_entities.constraints.abstracts.dynamic_constraint import DynamicConstraint
+from pipeline_entities.data_transfer.pipeline_data import PipelineData
+from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
+from pipeline_entities.pipeline_input.pipeline_input_data import PipelineInputData
+
+
+class InputKeyRequiredConstraint(DynamicConstraint):
+    _key_: str
+
+
+    def __init__(self, key: str) -> None:
+        if key.startswith("_") and key.endswith("_"):
+            self._key_ = key[1:-1]
+        else:
+            self._key_ = key
+
+
+
+    def evaluate(self, pipeline_data: PipelineData, pipeline_input: PipelineInput) -> bool:
+        if any(field.name == self._key_ for field in fields(PipelineInputData)):
+            transformed_key = f"_{self._key_}_"
+            return getattr(pipeline_input, transformed_key)
+        else:
+            return self._key_ in pipeline_input.additional_values or self._key_ in pipeline_input.additional_directly_injected_values
+
+
+
