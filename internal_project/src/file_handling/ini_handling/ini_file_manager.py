868de9909cd3989670971c3f4641be5601be5954
JannikGabler <jannik@mail-gabler.de>
2025-07-02 18:12:52 +0200

    [I]: Updated the ini_file_manager.py to the new ini format * Now uses ini_format_utils

--- internal_project/src/file_handling/ini_handling/ini_file_manager.py
+++ internal_project/src/file_handling/ini_handling/ini_file_manager.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from exceptions.duplicate_value_error import DuplicateValueError
+from exceptions.duplicate_value_error import DuplicateError
 from exceptions.not_instantiable_error import NotInstantiableError
 from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
 from pipeline_entities.pipeline_input.pipeline_input_data import PipelineInputData
+from utils.ini_format_utils import INIFormatUtils
 
 
 class INIFileManager:
@@ -20,12 +21,35 @@ class INIFileManager:
     ### Public methods ###
     ######################
     @staticmethod
-    def load_file(path: Path) -> dict[str, str]:
+    def load_file_as_entry_list(path: Path) -> list[str]:
         if not (path.is_file() and path.exists()):
             raise FileNotFoundError(f"File '{str(path)}' does not exists.")
 
-        entry_list: list[str] = INIFileManager._collect_entries_from_file_(path)
-        return INIFileManager._split_entry_list_into_dict_(entry_list)
+        file_content: str
+        with open(path, 'r') as file:
+            file_content: str = file.read()
+
+        return INIFormatUtils.split_into_entries(file_content)
+
+
+
+    @staticmethod
+    def load_file_as_key_value_pairs(path: Path) -> dict[str, str]:
+        if not (path.is_file() and path.exists()):
+            raise FileNotFoundError(f"File '{str(path)}' does not exists.")
+
+        entries: list[str] = INIFileManager.load_file_as_entry_list(path)
+        return INIFormatUtils.split_entries_into_key_value_pairs(entries)
+
+
+
+    # @staticmethod
+    # def load_file(path: Path) -> dict[str, str]:
+    #     if not (path.is_file() and path.exists()):
+    #         raise FileNotFoundError(f"File '{str(path)}' does not exists.")
+    #
+    #     entry_list: list[str] = INIFileManager._collect_entries_from_file_(path)
+    #     return INIFileManager._split_entry_list_into_dict_(entry_list)
 
 
 
@@ -39,60 +63,41 @@ class INIFileManager:
     #######################
     ### Private methods ###
     #######################
-    @staticmethod
-    def _collect_entries_from_file_(path: Path) -> list[str]:
-        entry_strings: list[str] = []
-        current_string: str = ""
+    # @staticmethod
+    # def _collect_entries_from_file_(path: Path) -> list[str]:
+    #     entry_strings: list[str] = []
+    #     current_string: str = ""
+    #
+    #     with open(path, 'r', encoding='utf-8') as file:
+    #         for line_number, raw_line in enumerate(file, start=1):
+    #             current_string, add_to_entries = INIFileManager._process_line_while_collecting_(raw_line, current_string)
+    #
+    #             if add_to_entries:
+    #                 entry_strings.append(current_string)
+    #                 current_string = ""
+    #
+    #     return entry_strings
+    #
+    #
+    #
+    # @staticmethod
+    # def _process_line_while_collecting_(raw_line: str, current_accumulation: str) -> tuple[str, bool]:
+    #     leading_stripped: str = raw_line.strip()
+    #     trailing_stripped: str = raw_line.rstrip()
+    #
+    #     if not leading_stripped.startswith(('#', '//', '[')) and not raw_line.isspace():
+    #         if current_accumulation:
+    #             string_to_add: str = "\n" + trailing_stripped
+    #         else:
+    #             string_to_add: str = raw_line.strip()
+    #
+    #         if string_to_add.endswith('\\'):
+    #             return current_accumulation + string_to_add[:-1].rstrip(), False
+    #         else:
+    #             return current_accumulation + string_to_add, True
+    #     else:
+    #         return current_accumulation, False
 
-        with open(path, 'r', encoding='utf-8') as file:
-            for line_number, raw_line in enumerate(file, start=1):
-                current_string, add_to_entries = INIFileManager._process_line_while_collecting_(raw_line, current_string)
-
-                if add_to_entries:
-                    entry_strings.append(current_string)
-                    current_string = ""
-
-        return entry_strings
 
 
 
-    @staticmethod
-    def _process_line_while_collecting_(raw_line: str, current_accumulation: str) -> tuple[str, bool]:
-        leading_stripped: str = raw_line.strip()
-        trailing_stripped: str = raw_line.rstrip()
-
-        if not leading_stripped.startswith(('#', '//', '[')) and not raw_line.isspace():
-            if current_accumulation:
-                string_to_add: str = "\n" + trailing_stripped
-            else:
-                string_to_add: str = raw_line.strip()
-
-            if string_to_add.endswith('\\'):
-                return current_accumulation + string_to_add[:-1].rstrip(), False
-            else:
-                return current_accumulation + string_to_add, True
-        else:
-            return current_accumulation, False
-
-
-
-    @staticmethod
-    def _split_entry_list_into_dict_(entry_list: list[str]) -> dict[str, str]:
-        entry_dict: dict[str, str] = {}
-
-        for entry in entry_list:
-            if "=" not in entry:
-                raise ValueError(f"The entry '{entry}' is invalid.")
-
-            splittings = entry.split("=", maxsplit=1)
-            key: str = splittings[0].rstrip()
-            value: str = splittings[1].lstrip()
-
-            if key in entry_dict:
-                raise DuplicateValueError(f"There are multiple entries with the same key '{key}'.")
-            if key.isspace():
-                raise ValueError(f"The key cannot be empty.")
-
-            entry_dict[key] = value
-
-        return entry_dict
