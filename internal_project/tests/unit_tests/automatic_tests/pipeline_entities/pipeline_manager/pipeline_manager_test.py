51810ffa8056e28237ca59544014b83255273142
JannikGabler <jannik@mail-gabler.de>
2025-07-02 09:33:01 +0200

    [I]: Merged 2 testfiles for the pipeline_manager into 1

--- internal_project/tests/unit_tests/automatic_tests/pipeline_entities/pipeline_manager/pipeline_manager_test.py
+++ internal_project/tests/unit_tests/automatic_tests/pipeline_entities/pipeline_manager/pipeline_manager_test.py
@@ -38,5 +38,37 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, False)  # add assertion here
 
 
+    def test_equidistant_node_generation_pipeline(self):
+        pipeline_configuration_data: PipelineConfigurationData = PipelineConfigurationData()
+        pipeline_configuration_data.supported_program_version = "Version(\"1.0.0\")"
+        pipeline_configuration_data.components = "Tree(\"\"\"BaseInput\n Equidistant Node Generator\n\"\"\")"
+
+        pipeline_configuration: PipelineConfiguration = PipelineConfiguration(pipeline_configuration_data)
+
+        pipeline_input_data: PipelineInputData = PipelineInputData()
+        pipeline_input_data.data_type = "jax.numpy.float32"
+        pipeline_input_data.node_count = "5"
+        pipeline_input_data.interpolation_interval = "jax.numpy.array([-1, 1])"
+
+        pipeline_input: PipelineInput = PipelineInput(pipeline_input_data)
+
+        pipeline: Pipeline = PipelineBuilder.build(pipeline_configuration, pipeline_input)
+        pipeline_manager: PipelineManager = PipelineManager(pipeline)
+
+        pipeline_manager.execute_all()
+
+        final_pipeline_data: PipelineData = pipeline_manager._pipeline_data_dict_["/0/"]
+
+        self.assertEqual(jnp.float32, final_pipeline_data.data_type)
+        self.assertEqual(5, final_pipeline_data.node_count)
+        self.assertTrue(jnp.array_equal(jnp.array([-1, 1]), final_pipeline_data.interpolation_interval))
+        self.assertIsNone(final_pipeline_data.function_values)
+
+        self.assertTrue(jnp.array_equal(jnp.array([-1, -0.5, 0, 0.5, 1]), final_pipeline_data.nodes))
+
+        self.assertEqual({}, final_pipeline_data.additional_values)
+
+
+
 if __name__ == '__main__':
     unittest.main()
