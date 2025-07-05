a23392fa43ac1bf93c63533ed9ba4320f48f3c4e
JannikGabler <jannik@mail-gabler.de>
2025-07-05 12:52:56 +0200

    [I]: Upgraded base_input_pipeline_component.py to the new input format

--- internal_project/src/pipeline_entities/components/default_components/default_input_components/base_input_pipeline_component.py
+++ internal_project/src/pipeline_entities/components/default_components/default_input_components/base_input_pipeline_component.py
@@ -2,18 +2,24 @@ from pipeline_entities.component_meta_info.default_component_meta_infos.input_co
     base_input_pipeline_component_meta_info
 from pipeline_entities.components.abstracts.input_pipeline_component import InputPipelineComponent
 from pipeline_entities.components.decorators.pipeline_component import pipeline_component
+from pipeline_entities.data_transfer.pipeline_data import PipelineData
+from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
 
 
-@pipeline_component(id="BaseInput", type=InputPipelineComponent, meta_info=base_input_pipeline_component_meta_info)
+@pipeline_component(id="Base Input", type=InputPipelineComponent, meta_info=base_input_pipeline_component_meta_info)
 class BaseInputPipelineComponent(InputPipelineComponent):
 
-    def perform_action(self) -> None:
-        self._pipeline_data_.data_type = self._pipeline_input_.data_type
-        self._pipeline_data_.node_count = self._pipeline_input_.node_count
-        self._pipeline_data_.interpolation_interval = self._pipeline_input_.interpolation_interval
+    def perform_action(self) -> PipelineData:
+        pipeline_data: PipelineData = self._pipeline_data_[0]
+        pipeline_input: PipelineInput = self._additional_execution_info_.pipeline_input
 
-        for key, value in self._pipeline_input_.additional_directly_injected_values.items():
-            self._pipeline_data_.additional_values[key] = value
+        pipeline_data.data_type = pipeline_input.data_type
+        pipeline_data.node_count = pipeline_input.node_count
+        pipeline_data.interpolation_interval = pipeline_input.interpolation_interval
 
+        for key, value in pipeline_input.additional_directly_injected_values.items():
+            pipeline_data.additional_values[key] = value
+
+        return pipeline_data
 
 
