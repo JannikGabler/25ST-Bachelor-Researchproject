6d3c520648b4851315db128345728c58c75d4b61
JannikGabler <jannik@mail-gabler.de>
2025-07-05 17:37:37 +0200

    [I]: Adjusted piecewise_function_expression_input_component.py to the new input format

--- internal_project/src/pipeline_entities/component_meta_info/default_component_meta_infos/input_components/piecewise_function_expression_input_component_meta_info.py
+++ internal_project/src/pipeline_entities/component_meta_info/default_component_meta_infos/input_components/piecewise_function_expression_input_component_meta_info.py
@@ -3,15 +3,16 @@ from pipeline_entities.constraints.default_constraints.default_dynamic_constrain
     InputKeyRequiredConstraint
 from pipeline_entities.constraints.default_constraints.default_static_constraints.attribute_required_constraint import \
     AttributeRequiredConstraint
+from pipeline_entities.constraints.default_constraints.default_static_constraints.max_predecessors_constraint import \
+    MaxPredecessorsConstraint
+from pipeline_entities.constraints.default_constraints.default_static_constraints.min_predecessors_constraint import \
+    MinPredecessorsConstraint
 
-multiple_function_expressions_input_component_meta_info: ComponentMetaInfo = ComponentMetaInfo(
-    attributes_modifying={"function_values"},
+piecewise_function_expression_input_component_meta_info: ComponentMetaInfo = ComponentMetaInfo(
+    attributes_modifying={"function_callable"},
 
     dynamic_constraints=[InputKeyRequiredConstraint("multiple_function_expressions"),
                          InputKeyRequiredConstraint("sympy_function_expression_simplification")],
 
-    static_constraints=[AttributeRequiredConstraint("data_type"), AttributeRequiredConstraint("node_count"),
-                        AttributeRequiredConstraint("nodes")],
-    
-    #mixed_constraints=[]
+    static_constraints=[MinPredecessorsConstraint(1), MaxPredecessorsConstraint(1),],
 )
---
+++ internal_project/src/pipeline_entities/components/default_components/default_input_components/piecewise_function_expression_input_component.py
@@ -0,0 +1,60 @@
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+import sympy
+from sympy import Expr
+
+from pipeline_entities.component_meta_info.default_component_meta_infos.input_components.piecewise_function_expression_input_component_meta_info import \
+    piecewise_function_expression_input_component_meta_info
+from pipeline_entities.components.abstracts.input_pipeline_component import InputPipelineComponent
+from pipeline_entities.components.decorators.pipeline_component import pipeline_component
+from pipeline_entities.data_transfer.additional_component_execution_data import AdditionalComponentExecutionData
+from pipeline_entities.data_transfer.pipeline_data import PipelineData
+from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
+
+
+@pipeline_component(id="piecewise function expression input", type=InputPipelineComponent, meta_info=piecewise_function_expression_input_component_meta_info)
+class PiecewiseFunctionExpressionInputComponent(InputPipelineComponent):
+
+    ##########################
+    ### Overridden methods ###
+    ##########################
+    def perform_action(self) -> PipelineData:
+        pipeline_data: PipelineData = self._pipeline_data_[0]
+
+        function_callable: callable = self._create_jax_lambda_()
+
+        pipeline_data.function_callable = function_callable
+        return pipeline_data
+
+
+
+    #######################
+    ### Private methods ###
+    #######################
+    def _create_jax_lambda_(self) -> callable:
+        x = sympy.symbols("x")
+        piecewise_sympy_expression: Expr = self._create_piecewise_sympy_expression(x)
+
+        lambda_scalar = sympy.lambdify(x, piecewise_sympy_expression, modules="jax")
+
+        return jax.vmap(lambda_scalar)
+
+
+
+    def _create_piecewise_sympy_expression(self, x: Any) -> Expr:
+        pipeline_input: PipelineInput = self._additional_execution_info_.pipeline_input
+
+        function_expressions: list[tuple[tuple[float, float], str]] = pipeline_input.multiple_function_expressions
+        simplify_expression: bool = pipeline_input.sympy_function_expression_simplification
+
+        expressions: list[tuple[Expr, any]] = []
+
+        for (lower, upper), function_expression in function_expressions:
+            expression: Expr = sympy.sympify(function_expression, evaluate=simplify_expression)
+            condition = (x >= lower) & (x < upper)
+            expressions.append((expression, condition))
+
+        return sympy.Piecewise(*expressions)
+
