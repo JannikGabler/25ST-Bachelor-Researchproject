cabf650cd209717774c60ab25a51942293e06d99
JannikGabler <jannik@mail-gabler.de>
2025-07-05 17:03:54 +0200

    [I]: Adjusted input_pipeline_component.py to the new input format * Removed functionality of the class

--- internal_project/src/pipeline_entities/components/abstracts/input_pipeline_component.py
+++ internal_project/src/pipeline_entities/components/abstracts/input_pipeline_component.py
@@ -1,16 +1,17 @@
+from abc import ABC
+
 from pipeline_entities.components.abstracts.pipeline_component import PipelineComponent
-from pipeline_entities.components.meta_classes.pipeline_component_meta import PipelineComponentMeta
-from pipeline_entities.data_transfer.pipeline_data import PipelineData
-from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
 
 
-class InputPipelineComponent(PipelineComponent, metaclass=PipelineComponentMeta):
-    _pipeline_input_: PipelineInput
+class InputPipelineComponent(PipelineComponent, ABC):
+    pass
 
-
-
-    def __init__(self, pipeline_input: PipelineInput, pipeline_data: PipelineData):
-        super().__init__(pipeline_data)
-        self._pipeline_input_ = pipeline_input
+    # _pipeline_input_: PipelineInput
+    #
+    #
+    #
+    # def __init__(self, pipeline_input: PipelineInput, pipeline_data: PipelineData):
+    #     super().__init__(pipeline_data)
+    #     self._pipeline_input_ = pipeline_input
 
 
