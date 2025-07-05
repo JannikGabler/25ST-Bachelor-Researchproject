88fdd88274401bc5d248b0f05b59760134fc0e82
JJCooper0 <jonathan@lipps.de>
2025-07-05 14:31:35 +0200

    added warning and skip option

--- cli_project/src/cli_launcher/cli.py
+++ cli_project/src/cli_launcher/cli.py
@@ -1,20 +1,18 @@
-# cli_project/src/cli_project/cli.py
 import argparse
 import sys
 
 from pathlib import Path
 
+from setup_manager.internal_logic_setup_manager import InternalLogicSetupManager
 from file_handling.pipeline_input_handling.pipeline_input_file_manager import PipelineInputFileManager
 from pipeline_entities.pipeline_configuration.dataclasses.pipeline_configuration_data import PipelineConfigurationData
-from file_handling.pipeline_configuration_handling.pipeline_configuration_file_manager import \
-    PipelineConfigurationFileManager
+from file_handling.pipeline_configuration_handling.pipeline_configuration_file_manager import PipelineConfigurationFileManager
 from pipeline_entities.pipeline_configuration.dataclasses.pipeline_configuration import PipelineConfiguration
 from pipeline_entities.pipeline_input.pipeline_input import PipelineInput
 from pipeline_entities.pipeline_input.pipeline_input_data import PipelineInputData
 from pipeline_entities.pipeline.pipeline import Pipeline
 from pipeline_entities.pipeline_builder.pipeline_builder import PipelineBuilder
 from pipeline_entities.pipeline_manager.pipeline_manager import PipelineManager
-from setup_manager.internal_logic_setup_manager import InternalLogicSetupManager
 
 def main():
     print("setting up internal logic...")
@@ -34,9 +32,20 @@ def main():
         "-pi", "--pipeline-input", required=True,
         help="Path to pipeline input file (.ini)"
     )
+    parser.add_argument(
+        "--skip-trust-warning", action="store_true",
+        help="Skip the warning prompt for trusting the files"
+    )
     args = parser.parse_args()
 
-    print("parsed arguments:", args)
+    if not args.skip_trust_warning:
+        print("Warning! Loading .ini files may execute arbitrary code. Only use pipeline config and input files from trusted sources!", file=sys.stderr)
+        reply = input("Do you trust these files? Proceed [y/N] ").strip().lower()
+        if reply not in ("y", "yes"):
+            print("Aborting.", file=sys.stderr)
+            sys.exit(1)
+
+        print("parsed arguments:", args)
 
     try:
         pcd_path = Path(args.pipeline_config)
@@ -59,4 +68,3 @@ def main():
     except Exception as e:
         print(f"ERROR: {e}", file=sys.stderr)
         raise e
-        # sys.exit(1)
