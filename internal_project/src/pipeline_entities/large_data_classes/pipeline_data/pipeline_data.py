ce963e2dded225fba55bba2130c39a3b7a83acba
JannikGabler <jannik@mail-gabler.de>
2025-07-18 22:51:41 +0200

    [I] Added evaluation_points to pipeline_data.py

--- internal_project/src/pipeline_entities/large_data_classes/pipeline_data/pipeline_data.py
+++ internal_project/src/pipeline_entities/large_data_classes/pipeline_data/pipeline_data.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from typing import Optional, Any, Callable
 import jax.numpy as jnp
 
-from interpolants.abstracts.interpolant import Interpolant
+from interpolants.abstracts.compilable_interpolant import CompilableInterpolant
 
 
 @dataclass
@@ -10,13 +10,14 @@ class PipelineData:
     data_type: type | None = None
     node_count: int | None = None
     interpolation_interval: jnp.ndarray | None = None
+    evaluation_points: jnp.ndarray | None = None
 
     function_callable: Callable[[jnp.ndarray], jnp.ndarray] | None = None   # Not Jax compiled, must be compiled by using components
 
     interpolation_nodes: jnp.ndarray | None = None
     interpolation_values: jnp.ndarray | None = None
 
-    interpolant: Optional[Interpolant] = None
+    interpolant: Optional[CompilableInterpolant] = None
 
     additional_values: dict[str, Any] = field(default_factory=dict)
 
