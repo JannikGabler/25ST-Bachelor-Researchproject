521476fe9421216f8b621c24a11a987391b7d20f
JannikGabler <jannik@mail-gabler.de>
2025-07-18 22:55:17 +0200

    [I] Added interpolant_values to pipeline_data.py

--- internal_project/src/pipeline_entities/large_data_classes/pipeline_data/pipeline_data.py
+++ internal_project/src/pipeline_entities/large_data_classes/pipeline_data/pipeline_data.py
@@ -10,7 +10,7 @@ class PipelineData:
     data_type: jnp.dtype | None = None
     node_count: int | None = None
     interpolation_interval: jnp.ndarray | None = None
-    evaluation_points: jnp.ndarray | None = None
+    interpolant_evaluation_points: jnp.ndarray | None = None
 
     function_callable: Callable[[jnp.ndarray], jnp.ndarray] | None = None   # Not Jax compiled, must be compiled by using components
 
@@ -18,6 +18,7 @@ class PipelineData:
     interpolation_values: jnp.ndarray | None = None
 
     interpolant: Optional[CompilableInterpolant] = None
+    interpolant_values: jnp.ndarray | None = None
 
     additional_values: dict[str, Any] = field(default_factory=dict)
 
