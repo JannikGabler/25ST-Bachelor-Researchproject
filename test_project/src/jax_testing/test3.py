3600b54934b5f5821dce5bf7819edec6c83c31aa
JannikGabler <jannik@mail-gabler.de>
2025-07-21 14:03:49 +0200

    [T] Jax exploration * analysis_aitken_neville_interpolation_core.py * analysis_barycentric_reduced_memory_usage.py * test.py * test2.py * test3.py * test4.py * test5.py * test6.py

---
+++ test_project/src/jax_testing/test3.py
@@ -0,0 +1,10 @@
+import jax
+import jax.numpy as jnp
+
+nodes = jnp.array([[0, 1, 2, 3],
+                  [4, 5, 6, 7]])
+
+print(jnp.pad(nodes, ((1, 0), (0, 0))))
+
+nodes = nodes.transpose()
+print(nodes / jnp.array([1, 2]))
