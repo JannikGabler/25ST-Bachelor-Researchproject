558995a95e76038b614db21a1a3b51496bcf06be
JannikGabler <jannik@mail-gabler.de>
2025-07-21 14:05:20 +0200

    [I] Added new barycentric_second_chebyshev_interpolation_core.py * barycentric_second_chebyshev_interpolation_core.py * barycentric_second_chebyshev_interpolation_core_meta_info.py * Adjusted __register_default_interpolation_cores__.py accordingly

--- internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_interpolation_cores/__register_default_interpolation_cores__.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_interpolation_cores/__register_default_interpolation_cores__.py
@@ -4,5 +4,6 @@ def register():
     from . import newton_interpolation_core
     from . import barycentric_first_interpolation_core
     from . import barycentric_second_interpolation_core
+    from . import barycentric_second_chebyshev_interpolation_core
     from . import aitken_neville_interpolation_core
     from . import fft_interpolation_core
