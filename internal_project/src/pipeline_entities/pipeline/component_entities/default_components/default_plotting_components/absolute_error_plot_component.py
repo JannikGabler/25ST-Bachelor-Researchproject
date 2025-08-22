b8fb7910390ec8cb4af518e76dd695ed4e4f708d
JannikGabler <jannik@mail-gabler.de>
2025-08-22 18:38:45 +0200

    [I] Added absolute_error_plot_component_meta_info.py

--- internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_plotting_components/absolute_error_plot_component.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_plotting_components/absolute_error_plot_component.py
@@ -1,35 +1,56 @@
-import os
-import pickle
+import textwrap
+
+import dill
 import subprocess
 import sys
 import tempfile
 
-from constants.internal_logic_constants import InterpolantsPlotComponentConstants
-from pipeline_entities.large_data_classes.plotting_data.interpolants_plot_component_data import \
-    InterpolantsPlotComponentData
-from pipeline_entities.pipeline.component_entities.component_meta_info.defaults.plot_components.interpolants_plot_component_meta_info import \
-    interpolants_plot_component_meta_info
+from constants.internal_logic_constants import AbsoluteErrorPlotComponentConstants
+from pipeline_entities.pipeline.component_entities.component_meta_info.defaults.plot_components.absolute_error_plot_component_meta_info import \
+    absolut_error_plot_component_meta_info
 from pipeline_entities.pipeline.component_entities.default_component_types.interpolation_core import InterpolationCore
 
-from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component_decorator import pipeline_component
+from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component_decorator import \
+    pipeline_component
 from pipeline_entities.large_data_classes.pipeline_data.pipeline_data import PipelineData
-from utils.interpolants_plot_component_utils import InterpolantsPlotComponentUtils
+from utils.absolute_error_plot_component_utils import AbsoluteErrorPlotComponentUtils
 
 
-@pipeline_component(id="absolute error plotter", type=InterpolationCore, meta_info=interpolants_plot_component_meta_info)
+@pipeline_component(id="absolute error plotter", type=InterpolationCore, meta_info=absolut_error_plot_component_meta_info)
 class AbsoluteErrorPlotComponent(InterpolationCore):
+    SUB_PROCESS_CODE = textwrap.dedent("""
+        import os
+        import sys
+        import dill
+
+        from utils.absolute_error_plot_component_utils import AbsoluteErrorPlotComponentUtils
+
+
+        if __name__ == "__main__":
+            # Check if we run in the child process
+            if len(sys.argv) == 3 and sys.argv[1] == "--child":
+                data_file = sys.argv[2]
+
+                with open(data_file, "rb") as f:
+                    pipeline_data, additional_execution_info = dill.load(f)
+
+                try:
+                    os.remove(data_file)
+                except FileNotFoundError:
+                    pass
+
+                AbsoluteErrorPlotComponentUtils.plot_data(pipeline_data, additional_execution_info)""")
+
 
 
     ######################
     ### Public methods ###
     ######################
     def perform_action(self) -> PipelineData:
-        plot_data: InterpolantsPlotComponentData = InterpolantsPlotComponentData.create_from(self._pipeline_data_, self._additional_execution_info_)
-
-        if InterpolantsPlotComponentConstants.SHOW_PLOT_IN_SEPARATE_PROCESS:
-            self.start_sub_process(plot_data)
+        if AbsoluteErrorPlotComponentConstants.SHOW_PLOT_IN_SEPARATE_PROCESS:
+            self._start_sub_process_()
         else:
-            InterpolantsPlotComponentUtils.plot_data(plot_data)
+            AbsoluteErrorPlotComponentUtils.plot_data(self._pipeline_data_, self._additional_execution_info_)
 
         return self._pipeline_data_[0]
 
@@ -38,30 +59,10 @@ class AbsoluteErrorPlotComponent(InterpolationCore):
     #######################
     ### Private methods ###
     #######################
-    def _start_sub_process_(self, plot_data: InterpolantsPlotComponentData) -> None:
+    def _start_sub_process_(self) -> None:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pkl") as file:
-            pickle.dump(plot_data, file)
+            dill.dump((self._pipeline_data_, self._additional_execution_info_), file)
             data_file = file.name
 
-        # Eigenständigen Python-Prozess starten, der völlig unabhängig läuft
-        subprocess.Popen([sys.executable, __file__, "--child", data_file])
+        subprocess.Popen([sys.executable, "-c", self.SUB_PROCESS_CODE, "--child", data_file])
 
-
-
-if __name__ == "__main__":
-    # Check if we run in the child process
-    if len(sys.argv) == 3 and sys.argv[1] == "--child":
-        data_file = sys.argv[2]
-
-        with open(data_file, "rb") as f:
-            data = pickle.load(f)
-
-        if not isinstance(data, InterpolantsPlotComponentData):
-            raise TypeError(f"Unpickled data is not of type '{InterpolantsPlotComponentData.__name__}'.")
-
-        try:
-            os.remove(data_file)
-        except FileNotFoundError:
-            pass
-
-        InterpolantsPlotComponentUtils.plot_data(data)
