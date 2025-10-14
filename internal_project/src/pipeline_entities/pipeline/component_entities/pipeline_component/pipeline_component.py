5f992ee69d0101e4e8d1f24faf58a7660d075d96
Hasan Tuzcu <st182730@stud.uni-stuttgart.de>
2025-10-14 23:15:45 +0200

    Slightly changes

--- internal_project/src/pipeline_entities/pipeline/component_entities/pipeline_component/pipeline_component.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/pipeline_component/pipeline_component.py
@@ -2,25 +2,22 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component_meta import (
-    PipelineComponentMeta,
-)
-from pipeline_entities.pipeline_execution.dataclasses.additional_component_execution_data import (
-    AdditionalComponentExecutionData,
-)
+from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component_meta import PipelineComponentMeta
+from pipeline_entities.pipeline_execution.dataclasses.additional_component_execution_data import AdditionalComponentExecutionData
 from data_classes.pipeline_data.pipeline_data import PipelineData
 
 if TYPE_CHECKING:
-    from pipeline_entities.pipeline.component_entities.component_info.dataclasses.pipeline_component_info import (
-        PipelineComponentInfo,
-    )
+    from pipeline_entities.pipeline.component_entities.component_info.dataclasses.pipeline_component_info import PipelineComponentInfo
 
 
 class PipelineComponent(metaclass=PipelineComponentMeta):
+
+
     ###########################
     ### Attributes of class ###
     ###########################
     _info_: PipelineComponentInfo
+
 
     ###############################
     ### Attributes of instances ###
@@ -28,16 +25,15 @@ class PipelineComponent(metaclass=PipelineComponentMeta):
     _pipeline_data_: list[PipelineData]
     _additional_execution_info_: AdditionalComponentExecutionData
 
+
     ###################
     ### Constructor ###
     ###################
-    def __init__(
-        self,
-        pipeline_data: list[PipelineData],
-        additional_execution_info: AdditionalComponentExecutionData,
-    ) -> None:
+    def __init__(self, pipeline_data: list[PipelineData], additional_execution_info: AdditionalComponentExecutionData) -> None:
+
         self._pipeline_data_ = pipeline_data
         self._additional_execution_info_ = additional_execution_info
+
 
     ########################
     ### Abstract methods ###
@@ -46,9 +42,10 @@ class PipelineComponent(metaclass=PipelineComponentMeta):
     def perform_action(self) -> PipelineData:
         pass
 
+
     #########################
     ### Getters & setters ###
     #########################
     @property
     def info(self) -> PipelineComponentInfo:
-        return self._info_  # PipelineComponentInfo is immutable
+        return self._info_
--- internal_project/src/pipeline_entities/pipeline/component_entities/pipeline_component/pipeline_component_meta.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/pipeline_component/pipeline_component_meta.py
@@ -2,9 +2,7 @@ import inspect
 from abc import ABCMeta
 from typing import get_type_hints
 
-from pipeline_entities.pipeline_execution.dataclasses.additional_component_execution_data import (
-    AdditionalComponentExecutionData,
-)
+from pipeline_entities.pipeline_execution.dataclasses.additional_component_execution_data import AdditionalComponentExecutionData
 from data_classes.pipeline_data.pipeline_data import PipelineData
 
 
@@ -16,29 +14,13 @@ class PipelineComponentMeta(ABCMeta):
         if not inspect.isabstract(cls):
             init_method = cls.__init__
             signature = inspect.signature(init_method)
-            parameters = list(signature.parameters.values())[
-                1:
-            ]  # Skip 'self' parameter
+            parameters = list(signature.parameters.values())[1:]
 
-            # Usage of module name and class name to prevent circular import
-            expected_parameter_types = [
-                list[PipelineData],
-                AdditionalComponentExecutionData,
-            ]
+            expected_parameter_types = [list[PipelineData], AdditionalComponentExecutionData]
 
             hints = get_type_hints(init_method)
-            actual_parameter_types = [
-                hints.get(parameter.name, None) for parameter in parameters
-            ]
+            actual_parameter_types = [hints.get(parameter.name, None) for parameter in parameters]
 
-            if len(parameters) != len(expected_parameter_types) or any(
-                actual_type != expected_type
-                for actual_type, expected_type in zip(
-                    actual_parameter_types, expected_parameter_types
-                )
-            ):
-
-                raise TypeError(
-                    f"The subclass '{cls.__name__}' of PipelineComponent has a constructor with invalid parameters. "
-                    f"Expected types for the parameters are '{str(expected_parameter_types)}'."
-                )
+            if len(parameters) != len(expected_parameter_types) or any(actual_type != expected_type for actual_type, expected_type in zip(actual_parameter_types, expected_parameter_types)):
+                raise TypeError(f"The subclass '{cls.__name__}' of PipelineComponent has a constructor with invalid parameters. "
+                                f"Expected types for the parameters are '{str(expected_parameter_types)}'.")
