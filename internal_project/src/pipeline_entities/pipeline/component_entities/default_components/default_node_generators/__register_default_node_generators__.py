566afc9dd3ef624fad7b4c2103a0375a95a77a1d
Hasan Tuzcu <st182730@stud.uni-stuttgart.de>
2025-10-14 23:37:54 +0200

    Added documentation for default_node_generators

--- internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_node_generators/__register_default_node_generators__.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/default_components/default_node_generators/__register_default_node_generators__.py
@@ -2,6 +2,10 @@
 
 
 def register():
+    """
+    Register the node generators."
+    """
+
     from . import equidistant_node_generator
     from . import first_type_chebyshev_node_generator
     from . import second_type_chebyshev_node_generator
