7aada2eed05d8093026f630932eba39751d235b5
Hasan Tuzcu <st182730@stud.uni-stuttgart.de>
2025-10-14 20:03:22 +0200

    Added documentation for setup_manager in internal_project\src\setup_manager

--- internal_project/src/setup_manager/internal_logic_setup_manager.py
+++ internal_project/src/setup_manager/internal_logic_setup_manager.py
@@ -1,21 +1,31 @@
 from exceptions.not_instantiable_error import NotInstantiableError
-from pipeline_entities.pipeline.component_entities.component_registry.component_registry import (
-    ComponentRegistry,
-)
+from pipeline_entities.pipeline.component_entities.component_registry.component_registry import ComponentRegistry
 
 
 class InternalLogicSetupManager:
+    """
+    Utility helpers for initializing internal logic. This class is not meant to be instantiated.
+    """
+
+
     ###################
     ### Constructor ###
     ###################
     def __init__(self) -> None:
-        raise NotInstantiableError(
-            "The class 'InternalLogicSetupManager' can not be instantiated."
-        )
+        """
+        Raises:
+            NotInstantiableError: Always raised when initialized to indicate that this class is not meant to be instantiated.
+        """
+        raise NotInstantiableError("The class 'InternalLogicSetupManager' can not be instantiated.")
+
 
     ######################
     ### Public methods ###
     ######################
     @staticmethod
     def setup():
+        """
+        Register default pipeline components for internal logic initialization.
+        """
+
         ComponentRegistry.register_default_components()
