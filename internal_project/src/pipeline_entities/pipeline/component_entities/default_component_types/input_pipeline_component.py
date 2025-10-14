25a4b26709b606bbbeb1dc6b50e3ad6dc3d57d7d
Hasan Tuzcu <st182730@stud.uni-stuttgart.de>
2025-10-15 01:03:09 +0200

    Added documentation for default_component_types

--- internal_project/src/pipeline_entities/pipeline/component_entities/default_component_types/input_pipeline_component.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/default_component_types/input_pipeline_component.py
@@ -1,9 +1,11 @@
 from abc import ABC
-
-from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component import (
-    PipelineComponent,
-)
+from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component import PipelineComponent
 
 
 class InputPipelineComponent(PipelineComponent, ABC):
+    """
+    Abstract base class for input components in the pipeline.
+    Input components are responsible for injecting or transferring input data into the pipeline data structure.
+    """
+
     pass
--- internal_project/src/pipeline_entities/pipeline/component_entities/default_component_types/interpolation_core.py
+++ internal_project/src/pipeline_entities/pipeline/component_entities/default_component_types/interpolation_core.py
@@ -1,9 +1,11 @@
 from abc import ABC
-
-from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component import (
-    PipelineComponent,
-)
+from pipeline_entities.pipeline.component_entities.pipeline_component.pipeline_component import PipelineComponent
 
 
 class InterpolationCore(PipelineComponent, ABC):
+    """
+    Abstract base class for interpolation core components in the pipeline.
+    Interpolation cores are responsible for computing the coefficients, constructing interpolants based on provided data and attaching them to the pipeline data.
+    """
+
     pass
