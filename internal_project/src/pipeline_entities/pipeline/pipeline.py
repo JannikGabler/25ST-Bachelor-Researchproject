29c723ff0e9faf443607c00f18ea67b4a073c484
Hasan Tuzcu <st182730@stud.uni-stuttgart.de>
2025-10-14 20:08:05 +0200

    Added documentation for pipeline.py in internal_project\src\pipeline_entities\pipeline

--- internal_project/src/pipeline_entities/pipeline/pipeline.py
+++ internal_project/src/pipeline_entities/pipeline/pipeline.py
@@ -1,19 +1,30 @@
 from dataclasses import dataclass
 
-from data_classes.pipeline_configuration.pipeline_configuration import (
-    PipelineConfiguration,
-)
+from data_classes.pipeline_configuration.pipeline_configuration import PipelineConfiguration
 from data_classes.pipeline_input.pipeline_input import PipelineInput
 from utils.directional_acyclic_graph_utils import DirectionalAcyclicGraphUtils
 
 
 @dataclass(frozen=True)
 class Pipeline:
+    """
+    Immutable container for a pipeline configuration and its input.
+    Provides an ASCII visualization of the component graph.
+    """
+
     pipeline_configuration: PipelineConfiguration
     pipeline_input: PipelineInput
 
+
     def __repr__(self) -> str:
-        dag = self.pipeline_configuration.components
+        """
+        ASCII representation of the pipeline's component DAG.
+
+        Returns:
+            str: Formatted DAG with component names and IDs.
+        """
+
+        dag   = self.pipeline_configuration.components
         nodes = list(dag.topological_traversal())
 
         adj = {}
